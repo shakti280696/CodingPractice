67d3c8e9098f39be51740ad23269fb89181c60db
shakti <shakti.da.power@gmail.com>
2023-08-15 00:51:38 +0530

    second commit after adding comments

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,6 @@
 class Arrays{
     public static void main(String[] args){
+        //print items of array
         int[] arr = {1,2,3,4,5};
         for(int x=arr.length-1;x>=0;x--){
             System.out.println(arr[x]);
